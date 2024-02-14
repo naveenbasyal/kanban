ee7328b3a1c9f6d7970cbc7089d759c1ec62bbc0
Naveen Basyal <naveenbasyal.001@gmail.com>
2024-02-14 15:51:41 +0530

    fixed:global proect bug

--- client/src/pages/AllFeedbacks.js
+++ client/src/pages/AllFeedbacks.js
@@ -38,13 +38,16 @@ export default function AllFeedbacks() {
     const getAllFeedbacks = async () => {
       try {
         setLoading(true);
-        const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/api/feedback`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        });
+        const response = await fetch(
+          `${process.env.REACT_APP_SERVER_URL}/api/feedback`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
         const data = await response.json();
         console.log(data?.feedbacks);
         if (data?.feedbacks) {
@@ -75,40 +78,38 @@ export default function AllFeedbacks() {
         </div>
       )}
       {feedbacks?.length > 0 ? (
-        feedbacks?.map(
-          ({
-            userId: { profilePicture, username, email },
-            title,
-            message,
-            _id,
-          }) => (
-            <Accordion
-              key={_id}
-              open={open === _id}
-              icon={<Icon id={_id} open={open} />}
-              className="border-b-[1px] border-b-slate-200 dark:border-b-slate-700"
-            >
-              <AccordionHeader onClick={() => handleOpen(_id)}>
-                <div className=" flex items-center gap-6">
-                  <LazyLoadImage
-                    src={profilePicture }
-                    effect="blur"
-                    className="h-12 w-12 rounded-full object-cover"
-                    alt={username}
-                    title={username}
-                  />
-                  <span className="capitalize text-2xl text-purple dark:text-slate-300 font-medium">
-                    {title}
-                  </span>
-                </div>
-              </AccordionHeader>
+        feedbacks?.map(({ userId, title, message, _id }) => (
+          <Accordion
+            key={_id}
+            open={open === _id}
+            icon={<Icon id={_id} open={open} />}
+            className="border-b-[1px] border-b-slate-200 dark:border-b-slate-700"
+          >
+            <AccordionHeader onClick={() => handleOpen(_id)}>
+              <div className=" flex items-center gap-6">
+                <LazyLoadImage
+                  src={
+                    userId?.profilePicture ||
+                    `https://api.dicebear.com/7.x/bottts/svg?seed=${
+                      userId?.username?.toLowerCase()?.split(" ")[0]
+                    }`
+                  }
+                  effect="blur"
+                  className="h-12 w-12 rounded-full object-cover"
+                  alt={userId?.username}
+                  title={userId?.username}
+                />
+                <span className="capitalize text-2xl text-purple dark:text-slate-300 font-medium">
+                  {title}
+                </span>
+              </div>
+            </AccordionHeader>
 
-              <AccordionBody className="text-2xl px-3 py-5 bg-gray-100 dark:bg-slate-800 rounded-md text-gray-600 dark:text-slate-300">
-                {message}
-              </AccordionBody>
-            </Accordion>
-          )
-        )
+            <AccordionBody className="text-2xl px-3 py-5 bg-gray-100 dark:bg-slate-800 rounded-md text-gray-600 dark:text-slate-300">
+              {message}
+            </AccordionBody>
+          </Accordion>
+        ))
       ) : (
         <h1>No feedbacks available</h1>
       )}
